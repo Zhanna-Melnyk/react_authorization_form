c4cfa702c34c196bc47aeb1b9fe460512954867c
Zhanna-Melnyk <58272440+Zhanna-Melnyk@users.noreply.github.com>
2024-06-10 19:16:06 +0300

    Feature: create logic and UI for inputs component

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,14 +1,65 @@
-import React from 'react';
+import { Field } from 'formik';
+import { useState } from 'react';
+
+import './styles.css';
 
 type InputFieldProps = {
 	inputType: string;
 	placeholder: string;
+	name: string;
+	onFocus?: VoidFunction;
+	isFieldValid?: boolean;
+	isSubmitClicked: boolean;
+	validationError?: string;
+	touched?: boolean;
+	isHiddable: boolean;
 };
 
-const InputField = ({ inputType, placeholder }: InputFieldProps) => {
+const InputField = ({
+	inputType,
+	placeholder,
+	name,
+	onFocus,
+	isFieldValid,
+	isSubmitClicked,
+	validationError,
+	touched,
+	isHiddable,
+}: InputFieldProps) => {
+	const [isVisible, setIsVisible] = useState<boolean>(false);
+
+	const getInputStyles = () => {
+		if (isSubmitClicked) {
+			return `input ${isFieldValid ? 'valid' : 'invalid'}`;
+		}
+		return `input ${isFieldValid ? 'valid' : ''}`;
+	};
+
+	const toggleVisibility = () => {
+		setIsVisible((visibility) => !visibility);
+	};
+
 	return (
 		<div>
-			<input type={inputType} placeholder={placeholder} />
+			<div className="fieldBlock">
+				<Field
+					className={getInputStyles()}
+					name={name}
+					type={isVisible ? 'text' : inputType}
+					placeholder={placeholder}
+					onFocus={onFocus}
+				/>
+				{isHiddable && (
+					<img
+						className="visibilityToggle"
+						src={isVisible ? './show.svg' : './hide.svg'}
+						onClick={toggleVisibility}
+					/>
+				)}
+			</div>
+			{validationError && touched && isSubmitClicked ? (
+				<div className="validationError">{validationError}</div>
+			) : null}
 		</div>
 	);
 };
